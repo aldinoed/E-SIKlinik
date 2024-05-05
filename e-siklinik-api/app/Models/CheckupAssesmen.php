822112ecb84583ede24257d191f3c8601ee46781
= <=>
2024-05-05 22:59:46 +0700

    adding assesmen model

---
+++ e-siklinik-api/app/Models/CheckupAssesmen.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CheckupAssesmen extends Model
+{
+    use HasFactory;
+}
